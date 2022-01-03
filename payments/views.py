6cb666bd6a0b5e3dc93504bfbaf16931c34b710a
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-01-03 11:10:58 +0800

    stripe-integration

---
+++ payments/views.py
@@ -0,0 +1,36 @@
+from django.http.response import HttpResponseNotFound, JsonResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse, reverse_lazy
+from .models import *
+from django.views.generic import ListView, CreateView, DetailView, TemplateView
+import stripe
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+# Create your views here.
+
+class ProductListView(ListView):
+    pass
+
+
+class ProductCreateView(CreateView):
+    pass
+
+
+class ProductDetailView(DetailView):
+    pass
+
+@csrf_exempt
+def create_checkout_session(request, id):
+	pass
+
+
+class PaymentSuccessView(TemplateView):
+    pass
+
+class PaymentFailedView(TemplateView):
+    pass
+
+class OrderHistoryListView(ListView):
+    pass
